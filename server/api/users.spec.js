3e541572d82afc5c47e1ce0621e4be5c9f533d08
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-19 11:27:38 -0400

    chore(tests/user): replaced tests for user model; removed tests for user api routes

--- server/api/users.spec.js
+++
@@ -1,9 +0,0 @@
-/* global describe beforeEach it */
-
-const {expect} = require('chai')
-const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
-const User = db.model('user')
-
-describe('User routes', () => {}) // end describe('User routes')
