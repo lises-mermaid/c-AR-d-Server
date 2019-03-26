252654c1faa83218fd6c9e453247c952974527b0
Vanessa Chu <vanessa.chu@outlook.com>
2019-03-26 17:49:32 -0400

    feat(public/logo): display logo

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <img src="logo.png" />
     <nav>
       {isLoggedIn ? (
         <div>
