94dd52cf32d3755bc2d4671f3269eb1ec002913d
Ayumi Ono <44326133+ayumiono1223@users.noreply.github.com>
2019-03-27 12:09:59 -0400

    fix: removed unused methods fixed some XML

--- client/components/video-upload.js
+++ client/components/video-upload.js
@@ -13,46 +13,26 @@ class VideoUpload extends Component {
     evt.preventDefault()
     this.props.addVideo('video', this.uploadInput.files[0])
   }
-
-  stateChanger(videoSrc) {
-    console.log('state')
-    this.setState({fileURL: videoSrc})
-  }
-
-  readFile(file) {
-    if (file && file.type === 'video/mp4') {
-      var reader = new FileReader()
-
-      reader.onload = function(evt) {
-        this.stateChanger(evt.target.result)
-      }
-
-      reader.readAsDataURL(file)
-    }
-  }
-
   render() {
     return (
       <div>
         <h3>Upload a video</h3>
-        {/* { */}
-        {/* this.state.fileURL && this.uploadInput.files[0].lastModified === "video/mp4" ? */}
-        <div>
-          <video width="320" height="240" controls>
-            <source src={this.state.fileURL} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        {/* :
-          <br/>
-        } */}
+        { this.state.fileURL && this.uploadInput.files[0].lastModified === "video/mp4" ?
+          <div>
+            <video width="320" height="240" controls>
+              <source src={this.state.fileURL} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          : <br/>
+          
+        }
         <form onSubmit={this.handleUploadImage}>
           <input
             ref={ref => {
               this.uploadInput = ref
             }}
             type="file"
-            // onChange={() => this.readFile(this.uploadInput.files[0])}
           />
           <br />
           <div>
