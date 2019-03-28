69d81c1404c7b2ff9880c17355bd40f583a4c02f
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-28 11:41:37 -0400

    Feat(components): about us component

---
+++ client/components/about-us.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const AboutUs = props => (
+  <div>
+    <img
+      src="logo.png"
+      width="320"
+      alt="c-AR-d logo"
+      style="justify-content: center"
+    />
+    <h2>About Us</h2>
+    <br />
+    <p>
+      C-AR-d, is a web & mobile application, where users generate a custom card
+      to send or receive video that will be rendered in Augmented Reality. Using
+      this app, you can create a greeting card with a special video message,
+      your friends and family can use our mobile app to view the video in
+      Augmented Reality.
+    </p>
+    <br />
+    <p>Created by Vanessa Chu, Ashlynn Mesidor, Ayumi Ono, Jamila Toaha</p>
+  </div>
+)
+
+export default AboutUs
