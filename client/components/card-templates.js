89219f6883bb7ff0d46cbec36947a871d5a6926e
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-28 22:43:09 -0400

    Feat(cardtemplates): Adds Columns and Rows

--- client/components/card-templates.js
+++ client/components/card-templates.js
@@ -1,98 +1,125 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
 
 const CardTemplates = props => (
   <div>
     <h2>Select A Card</h2>
-    <h5>Birthday</h5>
-    <div>
-      {props.cardTemplates
-        .filter(cardTemplate => cardTemplate.occasion === 'Birthday')
-        .map(cardTemplate => (
-          <div
-            key={cardTemplate.id}
-            onClick={() =>
-              props.selectCardTemplate('cardTemplate', cardTemplate)
-            }
-          >
-            <img src={cardTemplate.picture} height="140" width="120" />
-          </div>
-        ))}
-    </div>
-    <h5>Wedding</h5>
-    <div>
-      {props.cardTemplates
-        .filter(cardTemplate => cardTemplate.occasion === 'Wedding')
-        .map(cardTemplate => (
-          <div
-            key={cardTemplate.id}
-            onClick={() =>
-              props.selectCardTemplate('cardTemplate', cardTemplate)
-            }
-          >
-            <img src={cardTemplate.picture} height="140" width="120" />
-          </div>
-        ))}
-    </div>
-    <h5>Retirement</h5>
-    <div>
-      {props.cardTemplates
-        .filter(cardTemplate => cardTemplate.occasion === 'Retirement')
-        .map(cardTemplate => (
-          <div
-            key={cardTemplate.id}
-            onClick={() =>
-              props.selectCardTemplate('cardTemplate', cardTemplate)
-            }
-          >
-            <img src={cardTemplate.picture} height="140" width="120" />
-          </div>
-        ))}
-    </div>
-    <h5>Baby Shower</h5>
-    <div>
-      {props.cardTemplates
-        .filter(cardTemplate => cardTemplate.occasion === 'Baby Shower')
-        .map(cardTemplate => (
-          <div
-            key={cardTemplate.id}
-            onClick={() =>
-              props.selectCardTemplate('cardTemplate', cardTemplate)
-            }
-          >
-            <img src={cardTemplate.picture} height="140" width="120" />
-          </div>
-        ))}
-    </div>
-    <h5>Anniversary</h5>
-    <div>
-      {props.cardTemplates
-        .filter(cardTemplate => cardTemplate.occasion === 'Anniversary')
-        .map(cardTemplate => (
-          <div
-            key={cardTemplate.id}
-            onClick={() =>
-              props.selectCardTemplate('cardTemplate', cardTemplate)
-            }
-          >
-            <img src={cardTemplate.picture} height="140" width="120" />
-          </div>
-        ))}
-    </div>
-    <h5>Farewell</h5>
-    <div>
-      {props.cardTemplates
-        .filter(cardTemplate => cardTemplate.occasion === 'Farewell')
-        .map(cardTemplate => (
-          <div
-            key={cardTemplate.id}
-            onClick={() =>
-              props.selectCardTemplate('cardTemplate', cardTemplate)
-            }
-          >
-            <img src={cardTemplate.picture} height="140" width="120" />
-          </div>
-        ))}
-    </div>
+    <Container>
+      <h5>Birthday</h5>
+      <div>
+        <Row>
+          {props.cardTemplates
+            .filter(cardTemplate => cardTemplate.occasion === 'Birthday')
+            .map(cardTemplate => (
+              <div
+                key={cardTemplate.id}
+                onClick={() =>
+                  props.selectCardTemplate('cardTemplate', cardTemplate)
+                }
+              >
+                <Col>
+                  <img src={cardTemplate.picture} height="140" width="120" />
+                </Col>
+              </div>
+            ))}
+        </Row>
+      </div>
+      <h5>Wedding</h5>
+      <div>
+        <Row>
+          {props.cardTemplates
+            .filter(cardTemplate => cardTemplate.occasion === 'Wedding')
+            .map(cardTemplate => (
+              <div
+                key={cardTemplate.id}
+                onClick={() =>
+                  props.selectCardTemplate('cardTemplate', cardTemplate)
+                }
+              >
+                <Col>
+                  <img src={cardTemplate.picture} height="140" width="120" />
+                </Col>
+              </div>
+            ))}
+        </Row>
+      </div>
+      <h5>Retirement</h5>
+      <div>
+        <Row>
+          {props.cardTemplates
+            .filter(cardTemplate => cardTemplate.occasion === 'Retirement')
+            .map(cardTemplate => (
+              <div
+                key={cardTemplate.id}
+                onClick={() =>
+                  props.selectCardTemplate('cardTemplate', cardTemplate)
+                }
+              >
+                <Col>
+                  <img src={cardTemplate.picture} height="140" width="120" />
+                </Col>
+              </div>
+            ))}
+        </Row>
+      </div>
+      <h5>Baby Shower</h5>
+      <div>
+        <Row>
+          {props.cardTemplates
+            .filter(cardTemplate => cardTemplate.occasion === 'Baby Shower')
+            .map(cardTemplate => (
+              <div
+                key={cardTemplate.id}
+                onClick={() =>
+                  props.selectCardTemplate('cardTemplate', cardTemplate)
+                }
+              >
+                <Col>
+                  <img src={cardTemplate.picture} height="140" width="120" />
+                </Col>
+              </div>
+            ))}
+        </Row>
+      </div>
+      <h5>Anniversary</h5>
+      <div>
+        <Row>
+          {props.cardTemplates
+            .filter(cardTemplate => cardTemplate.occasion === 'Anniversary')
+            .map(cardTemplate => (
+              <div
+                key={cardTemplate.id}
+                onClick={() =>
+                  props.selectCardTemplate('cardTemplate', cardTemplate)
+                }
+              >
+                <Col>
+                  <img src={cardTemplate.picture} height="140" width="120" />
+                </Col>
+              </div>
+            ))}
+        </Row>
+      </div>
+      <h5>Farewell</h5>
+      <div>
+        <Row>
+          {props.cardTemplates
+            .filter(cardTemplate => cardTemplate.occasion === 'Farewell')
+            .map(cardTemplate => (
+              <div
+                key={cardTemplate.id}
+                onClick={() =>
+                  props.selectCardTemplate('cardTemplate', cardTemplate)
+                }
+              >
+                <Col>
+                  <img src={cardTemplate.picture} height="140" width="120" />
+                </Col>
+              </div>
+            ))}
+        </Row>
+      </div>
+    </Container>
   </div>
 )
 
