56a663573705cc20662c006d70bf6b15105536f3
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-29 14:44:35 -0400

    Feat(card-templates/sent-cards): adds shadows to card templates

--- client/components/sent-cards.js
+++ client/components/sent-cards.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {getAllSentCardsThunk} from '../store'
 import {Link} from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+import Media from 'react-bootstrap/Media'
 
 class SentCards extends Component {
   componentDidMount() {
@@ -15,16 +16,26 @@ class SentCards extends Component {
         <h2>Card History</h2>
         <div>
           {this.props.sentCards.map(card => (
-            <div key={card.uuid}>
-              <img src={card.cardTemplate.picture} height="140" width="120" />
-              {card.message ? <p>message: "{card.message}"</p> : <br />}
-              <p>
-                <a href={card.video}>video</a>
-              </p>
-              <p>{card.createdAt.slice(0, 10)}</p>
-              <Link to={`/cards/${card.uuid}`}>
-                <Button>Print Your Card</Button>
-              </Link>
+            <div key={card.uuid} className="sent-cards">
+              <Media>
+                <img
+                  src={card.cardTemplate.picture}
+                  height="140"
+                  width="120"
+                  className="align-self-center mr-3"
+                />
+                <Media.Body>
+                  {card.message ? <p>message: "{card.message}"</p> : <br />}
+                  <p>
+                    <a href={card.video}>video</a>
+                  </p>
+                  <p>{card.createdAt.slice(0, 10)}</p>
+                  <Link to={`/cards/${card.uuid}`}>
+                    <Button>Print Your Card</Button>
+                  </Link>
+                </Media.Body>
+              </Media>
+              <br />
             </div>
           ))}
         </div>
