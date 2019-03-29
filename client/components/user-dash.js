198c68862a473a9016e1af23034b4b83fa8f9113
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-28 20:29:29 -0400

    Feat(components): user dashboard component

---
+++ client/components/user-dash.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {logout} from '../store'
+import {UserHome, SentCards} from '.'
+import {Tab, Row, Col, Nav} from 'react-bootstrap'
+
+const UserDashboard = props => (
+  <div>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="select-template">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="tabs" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="profile">Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="card-history">Card History</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="logout">
+                <a href="#" onClick={props.handleClick}>
+                  Logout
+                </a>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="profile">
+              <UserHome />
+            </Tab.Pane>
+            <Tab.Pane eventKey="card-history">
+              <SentCards />
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  </div>
+)
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleClick() {
+      dispatch(logout())
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(UserDashboard)
