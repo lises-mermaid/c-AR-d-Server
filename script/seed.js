f2c05a8d9e1136517f80b530b3b4c99d9190ecd4
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-04-03 16:43:46 -0400

    Feat(api/users): secures users api routes (#61)

--- script/seed.js
+++ script/seed.js
@@ -37,6 +37,12 @@ async function seed() {
       username: 'Bell',
       email: 'bell@email.com',
       password: '123'
+    }),
+    User.create({
+      username: 'Admin',
+      email: 'admin@email.com',
+      password: '123',
+      isAdmin: true
     })
   ])
 
